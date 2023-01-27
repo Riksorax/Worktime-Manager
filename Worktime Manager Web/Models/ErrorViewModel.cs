6d6097efb46e83947d7304fc4e706e28faa1c9bb
Riksorax <78565646+Riksorax@users.noreply.github.com>
2023-01-27 21:15:06 +0100

    Create Blazor web application

--- Worktime Manager Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Worktime_Manager_Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
