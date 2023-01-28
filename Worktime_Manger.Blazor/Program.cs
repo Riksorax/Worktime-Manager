741b89d9bf4226b263402702da5ce42d2ac438ee
Riksorax <78565646+Riksorax@users.noreply.github.com>
2023-01-28 23:09:48 +0100

    Docker MySQL Db hinzugefügt und Xamarin.Forms gelöscht um des dann wahrscheinlich durch MAUI zu ersetzten

--- Worktime_Manger.Blazor/Program.cs
+++ Worktime_Manger.Blazor/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Radzen;
 using Worktime_Manger.Blazor;
+using Worktime_Manger.Blazor.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var serverVersion = ServerVersion.AutoDetect(connectionString);
+builder.Services.AddDbContext<WorktimeManagerContext.UserContext>(options =>
+    options.UseMySql(connectionString, serverVersion));
 
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
